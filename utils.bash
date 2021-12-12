cafa47c039df83f51bf12dd439d6c44359c0ee03
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2021-12-12 18:10:33 +0300

    Add app:error library

--- utils.bash
+++ utils.bash
@@ -81,7 +81,7 @@ app_header() {
 
 app_check_site_path_exists_and_readable() {
     if [ ! -r "${1}" ]; then
-        app_error "Site path not exists or not readable: ${1}"
+        app:error:error "Site path not exists or not readable: ${1}"
     fi
 }
 
@@ -89,15 +89,15 @@ app_check_site_path_is_empty_and_writable() {
     local site_path="${1}"
 
     if ! [[ -d "${site_path}" ]]; then
-        app_error "Site path is not exists or not a directory: ${site_path}"
+        app:error:error "Site path is not exists or not a directory: ${site_path}"
     fi
 
     if ! [[ -z "$(command ls -A ${site_path})" ]]; then
-        app_error "Site path is not empty: ${site_path}"
+        app:error:error "Site path is not empty: ${site_path}"
     fi
 
     if ! [[ -w "${site_path}" ]]; then
-        app_error "Site path is not writable: ${site_path}"
+        app:error:error "Site path is not writable: ${site_path}"
     fi
 }
 
@@ -142,7 +142,7 @@ app_load_site_config_file()
     fi
 
     if [[ "$(wc -l <<<${config_file})" -gt 1 ]]; then
-        app_error 'More than one site config files present'
+        app:error:error 'More than one site config files present'
     fi
 
     config_file="$(realpath ${config_file})"
@@ -163,7 +163,7 @@ app_load_config() {
             database_name     ) _[database_name]="${value}" ;;
             database_user     ) _[database_user]="${value}" ;;
             database_password ) _[database_password]="${value}" ;;
-            * ) app_error "Invalid configuration key: ${key}." ;;
+            * ) app:error:error "Invalid configuration key: ${key}." ;;
         esac
     done
 }
@@ -188,60 +188,8 @@ password="${site_config[database_password]}"
 EOF
 }
 
-app_commands_list() {
-    for cmd in ${app_base_path}/commands/*; do
-        echo "${cmd##*/}"
-    done | sort
-}
-
 app_plugins_list() {
     command find "${app_plugins_path}" -maxdepth 1 -type f -executable
-}
-
-app_error_unknown_command() {
-    local message
-
-    printf -v message 'Unknown command: %s.\n\n%s' "${1}" "$(app_available_commands)"
-
-    app_error "${message}"
-}
-
-app_error_unknown_option() {
-    app_error "Unknown option: ${1}"
-}
-
-app_error_missing_argument() {
-    app_error "Missing argument${1:+: }${1:-}"
-}
-
-app_error_too_many_arguments() {
-    app_error "Too many arguments.${1:+ Expected: }${1:-}"
-}
-
-app_error() {
-    local exit_code=1
-
-    if [ "${1}" = '--exit' ]; then
-        exit_code="${2}"
-
-        if [[ "${exit_code}" -lt 1 || "${exit_code}" -gt 255 ]]; then
-            app_error "app_error(): invalid exit code: ${exit_code}" && return 1
-        fi
-
-        shift 2
-    fi
-
-    printf "${app_name}${app_name:+ }ERROR: %s\n" "$@" >&2
-
-    exit "${exit_code}"
-}
-
-app_available_commands() {
-    printf 'Available commands:\n\n'
-
-    for c in $(app_commands_list); do
-        printf '  %s\n' "${c}"
-    done
 }
 
 app_repeat_str() {
