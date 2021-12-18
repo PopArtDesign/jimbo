585e188f7ba1ef9cef0e14f56a62fca9ca1b24a2
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2021-12-19 02:23:39 +0300

    Add more tests for app::site::load_config

--- spec/lib/site_spec.sh
+++ spec/lib/site_spec.sh
@@ -13,15 +13,67 @@ Describe 'app::site::load_config'
 
     BeforeEach 'beforeEach'
 
+    It 'saves config values to special variable "site_config"'
+        Data:raw
+            #|root: /tmp
+            #|exclude: /cache
+            #|include: index.html
+        End
+
+        When call app::site::load_config 'base'
+
+        The status should be success
+        The value "${#site_config[@]}" should equal 3
+        The value "${site_config[root]:-}" should equal '/tmp'
+        The value "${site_config[exclude]:-}" should equal '/cache'
+        The value "${site_config[include]:-}" should equal 'index.html'
+    End
+
+    It 'appends "include" and "exclude"'
+        load_config() {
+            app::site::load_config 'base' <<CONFIG
+exclude: /cache .cache
+include: index.html
+CONFIG
+
+            app::site::load_config 'plugin' <<CONFIG
+exclude: *.zip .git
+include: *.php
+CONFIG
+        }
+
+        When call load_config
+
+        The status should be success
+        The value "${site_config[exclude]:-}" should equal '/cache .cache *.zip .git'
+        The value "${site_config[include]:-}" should equal 'index.html *.php'
+    End
+
+    It 'ignores empty lines'
+        Data:raw
+            #|
+            #|root: /tmp
+            #|   
+            #|
+            #|exclude: .git
+            #|
+        End
+
+        When call app::site::load_config 'base'
+
+        The status should be success
+        The value "${site_config[*]}" should equal '.git /tmp'
+    End
+
     It 'fails when invalid key is encountered'
         Data:raw
             #|root: /tmp
             #|foo: bar
         End
 
-        When call app::site::load_config 'base'
+        When call app::site::load_config '/usr/local/etc/jimbo/cool-site.conf'
 
         The status should be failure
-        The error should include 'base: invalid key: foo'
+        The error should include '/usr/local/etc/jimbo/cool-site.conf: invalid key: foo'
     End
 End
