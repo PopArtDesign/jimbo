ae81b8d0ec6d1d8e663a2f363df7389bf5b7e97d
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2021-12-18 17:05:52 +0300

    Move common code to separate file 'common.bash'

---
+++ common.bash
@@ -0,0 +1,15 @@
+set -euo pipefail
+shopt -s lastpipe nullglob
+
+declare -gA app_libs_loaded=()
+
+app::use() {
+    local lib="${1}"
+    local path="${app_libs_path}/${lib}.bash"
+
+    [[ -n "${app_libs_loaded[${lib}]:-}" ]] && return
+
+    source "${path}"
+
+    app_libs_loaded["${lib}"]="${path}"
+}
